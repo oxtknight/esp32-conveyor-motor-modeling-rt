d854be8eb797097b252c6a4e5e1d69d55cff8f41
oxtknight <claudyharu@gmail.com>
2026-07-09 18:41:50 +0100

    more edits brr

--- twin_motor.py
+++ twin_motor.py
@@ -17,21 +17,24 @@ class JBG37twin:
         
         return didt, dwdt
     
-    def step(self, voltage , dt = 0.01):
-        didt, dwdt = self.get_acc(voltage)
-        self.current += didt * dt
-        self.w += dwdt * dt #omaga im losing my marbles copying these equations from pdf fah
+    def step(self, voltage ,load_torque = 0.0, dt = 0.01, substeps=150):
+        dtsub= dt/ substeps
+        for _ in range(substeps):
+            didt, dwdt = self.get_acc(voltage,load_torque)
+            self.current += didt * dt
+            self.w += dwdt * dt #omaga im losing my marbles copying these equations from pdf fah
 
         #here i think ill add limitation/protection to not get negative speed , hear me out on this fellas
-        if self.w < 0 : self.w = 0
+            if self.w < 0:
+                self.w = 0
 
         conveyor_rpm = (self.w * 9.5493) / self.Ratio
         return conveyor_rpm , self.current
  
- if __name__ == "__main__":
+if __name__ == "__main__":
      motor = JBG37twin()
      print(" 24v start")
      for t in range(10): 
          rpm, amps = motor.step(24.0, dt= 0.01)
          print(f"time: {t*10}ms | speed: {rpm:.2f} rpm | current: {amps:.3f}A")
-
+ 
