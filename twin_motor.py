8f027354cb89f0058e2a38895a8d51d7112a6d59
oxtknight <claudyharu@gmail.com>
2026-07-09 19:06:16 +0100

    added folders and stuff

--- twin_motor.py
+++ twin_motor.py
@@ -1,13 +1,13 @@
 import numpy as np
-class JBG37twin:
+class JBG37twin:# i used the datasheet 
     def __init__(self):
-        self.R = 12.0  #resistance in ohms
-        self.L = 0.01 #inductance in henries
-        self.Kt = 0.5 #torque constant Nm/A
-        self.Ke = 0.5 #back EMF constant V/rad/s
-        self.J = 0.0001 #rotor inertia kg.m^2
-        self.B = 0.0001 #viscous friction
-        self.Ratio = 150 #this ios the gearbox ration from datasheet of the 60 rpm model
+        self.R = 30.0  #resistance in ohms
+        self.L = 0.005 #inductance in henries (we gonna tune this later on)
+        self.Kt = 0.0135 #torque constant Nm/A
+        self.Ke = 0.0135 #back EMF constant V/rad/s
+        self.J = 1e-6 #rotor inertia kg.m^2 (this as well gonna be tuned later)
+        self.B = 2.6e-6 #viscous friction
+        self.Ratio = 90 #this ios the gearbox ration from datasheet of the 60 rpm model
         self.w = 0.0 # so this is the angular velocity uk w  rad/s
         self.current = 0.0 # amps
     
@@ -33,8 +33,8 @@ class JBG37twin:
  
 if __name__ == "__main__":
      motor = JBG37twin()
-     print(" 24v start")
-     for t in range(10): 
-         rpm, amps = motor.step(24.0, dt= 0.01)
+     print(" 12v start")
+     for t in range(200): 
+         rpm, amps = motor.step(12.0, dt= 0.01)
          print(f"time: {t*10}ms | speed: {rpm:.2f} rpm | current: {amps:.3f}A")
  
