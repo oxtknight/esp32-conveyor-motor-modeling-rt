cee606e45f4691dfc44775c9727296f2058d6df5
Chmk <chmokeddem123@gmail.com>
2026-08-05 22:26:29 +0100

    added Interrupt and omitted current measurements
    Updated WiFi credentials and modified measurement sending logic to include voltage and speed only. Adjusted encoder handling and improved interrupt management.

--- ESP32_code.cpp
+++ ESP32_code.cpp
@@ -2,126 +2,119 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-#define WIFI_NETWORK "TK's M16"
-#define WIFI_PASSWORD "knight20072006"
+#define WIFI_NETWORK "inelecer"
+#define WIFI_PASSWORD "20301782"
 #define WIFI_TIMEOUT_MS 100000
 
-int count = 0;
+#define PIN_VOLTAGE 4
+#define PIN_ENCODER 25
+
+
+volatile uint32_t count = 0;
 float Real_Speed = 0;
 unsigned long passing_time = 0;
 unsigned long measurement_time = 0;
 unsigned long timing = 0;
 float Real_Voltage = 0;
 float Real_Current = 0;
-bool Encoder = LOW;
-bool previous_Encoder = LOW;
 int httpResponseCode = 0;
 
-void setup() {
-    Serial.begin(9600);
-    connectToWifi();
-    pinMode(25, INPUT);
+// Encoder pulses per revolution
+#define PPR 20
+
+void IRAM_ATTR encoderISR()
+{
+    count++;
 }
 
-void loop() {
 
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(PIN_VOLTAGE, INPUT);
+    pinMode(PIN_ENCODER, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(PIN_ENCODER),encoderISR, RISING);
+    connectToWifi();
+    Serial.println("System Started");
+}
+
+void loop()
+{
     timing = millis();
-
-    Real_Voltage = analogRead(A0) * (3.3 / 4095.0);
-    Real_Current = analogRead(A2) * (3.3 / 4095.0);
-
-    Encoder = digitalRead(25);
-
-    if (timing - passing_time <= 100) {
-
-        if (Encoder == HIGH && previous_Encoder == LOW) {
-            count++;
-        }
-
-        previous_Encoder = Encoder;
-
-    } else {
-
-        Real_Speed = 60.0 * count / (20.0 * 0.1);
-
+    if(timing - passing_time >= 100)
+    {
+        Real_Voltage =analogRead(PIN_VOLTAGE)* (3.3 / 4095.0);
+        noInterrupts();
+        uint32_t pulses = count;
+        count = 0;
+        interrupts();
+        Real_Speed = pulses * 60.0 /(PPR * 0.1);    
         passing_time = timing;
         measurement_time += 100;
-        count = 0;
-
-        httpResponseCode = sendMeasurement(
-            Real_Voltage,
-            Real_Current,
-            Real_Speed,
-            measurement_time
-        );
-
-        if (httpResponseCode > 0) {
-            Serial.print("HTTP Response: ");
-            Serial.println(httpResponseCode);
-        } else {
-            Serial.print("Error sending POST: ");
-            Serial.println(httpResponseCode);
-        }
-    }
+        httpResponseCode =
+        sendMeasurement(Real_Voltage, Real_Speed, measurement_time);
+        Serial.print("Voltage: ");
+        Serial.print(Real_Voltage);
+        Serial.print(" RPM: ");
+        Serial.print(Real_Speed);
+        Serial.print(" HTTP: ");
+        Serial.println(httpResponseCode);
+}
+delay(1);
 }
 
-void connectToWifi() {
 
-    Serial.print("Connecting to Wifi");
-
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
-
-    unsigned long start = millis();
-
-    while (WiFi.status() != WL_CONNECTED &&
-           millis() - start < WIFI_TIMEOUT_MS) {
-
-        Serial.print(".");
-        delay(100);
-    }
-
-    if (WiFi.status() != WL_CONNECTED) {
-        Serial.println(" Failed!");
-    } else {
-        Serial.print("Connected! ");
-        Serial.println(WiFi.localIP());
-    }
-}
-
-int sendMeasurement(float Voltage, float Current, float Speed, unsigned int timer)
+void connectToWifi()
 {
-    if (WiFi.status() == WL_CONNECTED) {
+Serial.print("Connecting to Wifi");
+WiFi.mode(WIFI_STA);
+WiFi.begin(WIFI_NETWORK,WIFI_PASSWORD);
 
-        HTTPClient http;
-        http.begin("http://10.52.32.25:5000/telemetry");
-        http.addHeader("Content-Type", "application/json");
+unsigned long start = millis();
 
-        StaticJsonDocument<256> doc;
+while( WiFi.status()!=WL_CONNECTED && millis()-start<WIFI_TIMEOUT_MS)
+{
+Serial.print(".");
+delay(100);
+}
 
-        doc["Vr"] = Voltage;
-        doc["Wr"] = Speed;
-        doc["time"] = timer;
+if(WiFi.status()!=WL_CONNECTED)
+{
+Serial.println(" Failed!");
+}
+else
+{
+Serial.print("Connected! ");
+Serial.println(WiFi.localIP());
+}
+}
 
-        String body;
-        serializeJson(doc, body);
 
-        Serial.print("Sending: ");
-        Serial.println(body);
+int sendMeasurement(float Voltage, float Speed, unsigned int timer)
+{
+if(WiFi.status()==WL_CONNECTED)
+{
+HTTPClient http;
+http.begin("http://10.52.32.25:5000/telemetry");
+http.addHeader("Content-Type","application/json");
 
-        int response = http.POST(body);
+StaticJsonDocument<256> doc;
+doc["Vr"] = Voltage;
+doc["Wr"] = Speed;
+doc["time"] = timer;
+String body;
+serializeJson(doc,body);
 
-        Serial.print("HTTP Response: ");
-        Serial.println(response);
+Serial.print("Sending: ");
+Serial.println(body);
 
-        if (response > 0) {
-            Serial.println(http.getString());
-        }
-
-        http.end();
-        return response;
-    }
-
-    Serial.println("WiFi disconnected");
-    return -1;
+int response =http.POST(body);
+if(response > 0)
+{
+Serial.println(http.getString());
+}
+http.end();
+return response;
+}
+return -1;
 }
